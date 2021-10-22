a94edefd089822e91ade310ecd4e198214e364f0
Hasan Zahran <hasanzahran@Hasans-MacBook-Pro.local>
2021-10-21 19:56:25 -0700

    fix util function

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const Iframe: React.FC<any> = ({iframeData}) => {
-
     return (
         <div className={"iframe-container flex"}>
-            {iframeData?.length > 0 && iframeData?.map((iframe: { width: string | number | undefined; height: string | number | undefined; src: string | undefined; }, index: any) => (
+            {iframeData?.length > 0 && iframeData?.map((iframe: { width: string; height: string; src: string }, index: number) => (
                 <div key={index + iframe.height} className={"iframe-card"}>
                     <iframe title={`iframe_${index}`} width={iframe.width} height={iframe.height} src={iframe.src} frameBorder="0" style={{left: iframe.width +'px'}} />
                 </div>
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,17 +1,16 @@
 export const MAIN_URL = `https://platform.pearmill.com/tests`;
 
 export const fixData = (data:any) => {
-    const tempItem:{}[] = [];
+    const iframeData = [];
     for (let item in data) {
-        let tempObj = {width: null,height:null,src:null};
+        let tempObj = {width:null,height:null,src:null};
         let srcWithQuotes = data[item].iframe_preview.match(/src=([^\s]*)\s/)[1];
         let width = data[item].iframe_preview.match(/width=([^\s]*)\s/)[1];
         let height = data[item].iframe_preview.match(/height=([^\s]*)\s/)[1];
-        let src = srcWithQuotes.substring(1,srcWithQuotes.length - 1);
         tempObj.width = width.substring(1,width.length - 1);
         tempObj.height = height.substring(1,height.length - 1);
-        tempObj.src = src.replace("amp;", "");
-        tempItem.push(tempObj);
+        tempObj.src = srcWithQuotes.substring(1,srcWithQuotes.length - 1).replace("amp;", "");
+        iframeData.push(tempObj);
     }
-    return tempItem;
+    return iframeData;
 }
